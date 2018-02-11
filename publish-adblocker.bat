989249282e627bcc13ff1994d3617ce281f946de
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-02-10 20:53:27 -0700

    firefox auto upload working woohoo

--- publish-adblocker.bat
+++ publish-adblocker.bat
@@ -4,7 +4,7 @@ SET /P CONFIRM=Publish Nano Adblocker [Y/N]?
 IF "%CONFIRM%" NEQ "Y" GOTO END
 
 NODE . --chromium --adblocker --pack --publish
-REM NODE . --firefox --adblocker --pack --publish
+NODE . --firefox --adblocker --pack --publish
 REM NODE . --edge --adblocker --pack --publish
 
 :END
