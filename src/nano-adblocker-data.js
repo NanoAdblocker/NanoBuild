17a49da89d0e01ca6b7a1df102cb95ec2a4d4b22
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-02-10 20:44:50 -0700

    Ops, forgot to .end() the request

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -18,7 +18,7 @@ exports.basedOn = "uBlock Origin Version/1.15.4 Commit/43eab7a Sidebar/disabled"
  * The version key.
  * @const {string}
  */
-exports.version = "1.0.0.29";
+exports.version = "1.0.0.30";
 
 /**
  * Extra information specific to Chrome.
