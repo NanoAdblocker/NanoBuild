c68d3e531c2ef1090e08d2af67186165a2a969eb
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-02-22 19:26:41 -0700

    publish hotfix

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -13,12 +13,12 @@ const assert = require("assert");
  * The based on string.
  * @const {string}
  */
-exports.basedOn = "uBlock Origin Version/1.15.6 Commit/aadcfcd Sidebar/disabled";
+exports.basedOn = "uBlock Origin Version/1.15.6 Commit/aadcfcd Sidebar/disabled HTMLFiltering/default-off";
 /**
  * The version key.
  * @const {string}
  */
-exports.version = "1.0.0.32";
+exports.version = "1.0.0.33";
 
 /**
  * Extra information specific to Chrome.
