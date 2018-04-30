34c9a3216b2fa7f564773ce7af6a7c0f2cce8630
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-04-30 16:09:25 -0600

    publish new build

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -13,7 +13,7 @@ const assert = require("assert");
  * The version number.
  * @const {string}
  */
-exports.version = "1.0.0.44";
+exports.version = "1.0.0.45";
 /**
  * The based on string.
  * @const {string}
