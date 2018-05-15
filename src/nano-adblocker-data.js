c5f2e065d45c1730365e29a55eabf6bff94fd997
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-05-15 10:06:19 -0600

    prepare new build

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -13,12 +13,12 @@ const assert = require("assert");
  * The version number.
  * @const {string}
  */
-exports.version = "1.0.0.48";
+exports.version = "1.0.0.49";
 /**
  * The based on string.
  * @const {string}
  */
-exports.basedOn = "uBlock Origin Version/1.16.4 Commit/62fd1fe UserCSS/disabled";
+exports.basedOn = "uBlock Origin Version/1.16.6 Commit/5edc527 UserCSS/disabled";
 
 /**
  * Extra information specific to Chromium.
