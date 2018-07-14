36837244150b3bf0c852f1f8c62350ba4af5d27c
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-14 14:35:05 -0600

    pull filters update

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -13,7 +13,7 @@ const assert = require("assert");
  * The version number.
  * @const {string}
  */
-exports.version = "1.0.0.55";
+exports.version = "1.0.0.56";
 /**
  * The based on string.
  * @const {string}
