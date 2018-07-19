881bba3071134614e00de410efa22446f6a54345
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2018-07-18 18:25:03 -0600

    ops forgot to pull filters

--- src/nano-adblocker-data.js
+++ src/nano-adblocker-data.js
@@ -13,7 +13,7 @@ const assert = require("assert");
  * The version number.
  * @const {string}
  */
-exports.version = "1.0.0.57";
+exports.version = "1.0.0.58";
 /**
  * The based on string.
  * @const {string}
