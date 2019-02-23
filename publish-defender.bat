2c82b70ad2a0d030840393c7231dd4ee9dc56397
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-02-23 12:01:08 -0700

    No commit message

--- publish-defender.bat
+++ publish-defender.bat
@@ -4,7 +4,10 @@ SET /P CONFIRM=Publish Nano Defender [Y/N]?
 IF "%CONFIRM%" NEQ "Y" GOTO END
 
 NODE . --chromium --defender --pack --publish
-NODE . --edge --defender --pack --publish
+
+REM NODE . --edge --defender --pack --publish
+NODE . --edge --defender --pack
+ECHO "Publishing for Edge is disabled"
 
 :END
 PAUSE
